9b545c251ce442b5b5834315060bce0426f7065a
fegler <ydjoo12@gmail.com>
2017-12-29 01:18:31 +0900

    binary classification with filename queue

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import tensorflow as tf
 import numpy as np
 tf.set_random_seed(777)
 
-xy = np.loadtxt('data-03-diabetes.csv', delimiter=',',dtype=np.float32)
 
-x_data = xy[:,0:-1]
-y_data = xy[:,[-1]]
+filename_queue = tf.train.string_input_producer(['data-03-diabetes.csv'],shuffle=False, name='filename_queue')
+
+reader = tf.TextLineReader()
+key,value = reader.read(filename_queue)
+
+record_defaults = [[0.],[0.],[0.],[0.],[0.],[0.],[0.],[0.],[0.]]
+xy = tf.decode_csv(value, record_defaults=record_defaults)
+
+train_x_batch, train_y_batch = tf.train.batch([xy[0:-1],xy[-1:]],batch_size=10)
+
 
 X = tf.placeholder(tf.float32,shape=[None,8])
 Y = tf.placeholder(tf.float32,shape=[None,1])
@@ -23,11 +30,16 @@ accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
     for step in range(20001):
+        x_data, y_data = sess.run([train_x_batch,train_y_batch])
         cost_val, _ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
         if step%200 == 0:
             print(step, cost_val)
 
+    coord.request_stop()
+    coord.join(threads)
     hy_val, predic_val, accu = sess.run([Hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data})
     print("HY_val: ", hy_val, "\nPredicted: ",predic_val, "\naccuracy: ",accu)
