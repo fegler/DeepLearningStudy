8351f6c2054e01274259a87b77742704470a9269
fegler <ydjoo12@gmail.com>
2017-12-29 01:02:17 +0900

    binary classification with diabetes.csv

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
+import tensorflow as tf
+import numpy as np
+tf.set_random_seed(777)
 
-class calculator:
-    def __init__(self):
-        self.result = 0
-    def setfirst(self, start):
-        self.result = start
-    def add(self, num):
-        self.result = self.result + num
-    def sub(self, num):
-        self.result = self.result - num
-    def mul(self, num):
-        self.result = self.result * num
-    def div(self, num):
-        self.result = self.result / num
-    def answer(self):
-        return self.result
+xy = np.loadtxt('data-03-diabetes.csv', delimiter=',',dtype=np.float32)
 
-cal1 = calculator()
+x_data = xy[:,0:-1]
+y_data = xy[:,[-1]]
 
-for i in range(1,4):
-    print("menu: 1.set first value   2.add   3.sub   4.mul   5.div")
-    menu = int(input())
-    print("input number")
-    number = int(input())
+X = tf.placeholder(tf.float32,shape=[None,8])
+Y = tf.placeholder(tf.float32,shape=[None,1])
 
-    if menu == 1:
-        cal1.setfirst(number)
-    elif menu == 2:
-        cal1.add(number)
-    elif menu == 3:
-        cal1.sub(number)
-    elif menu == 4:
-        cal1.mul(number)
-    else:
-        cal1.div(number)
+W = tf.Variable(tf.random_normal([8,1]),name='weight')
+B = tf.Variable(tf.random_normal([1]),name='bias')
 
-ans = cal1.answer()
-print(ans, type(ans))
+Hypothesis = tf.sigmoid(tf.matmul(X,W)+B,name='Hypothesis')
+cost = -tf.reduce_mean(Y*tf.log(Hypothesis) + (1-Y)*tf.log(1-Hypothesis))
+
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+
+predicted = tf.cast(Hypothesis>0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(20001):
+        cost_val, _ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
+        if step%200 == 0:
+            print(step, cost_val)
+
+    hy_val, predic_val, accu = sess.run([Hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data})
+    print("HY_val: ", hy_val, "\nPredicted: ",predic_val, "\naccuracy: ",accu)
